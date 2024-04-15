e2f515320546760a08a9d8460f07cfd9b45350ec
mac <igorv8836@mail.ru>
2024-04-15 13:27:03 +0300

    adding viewmodels and small changes in ui

---
+++ app/src/main/java/com/example/educationgallery/ui/components/ScheduleFragmentScreen.kt
@@ -0,0 +1,9 @@
+package com.example.educationgallery.ui.components
+
+import androidx.compose.runtime.Composable
+import com.example.educationgallery.viewmodels.ScheduleViewModel
+
+@Composable
+fun ScheduleFragmentScreen(viewModel: ScheduleViewModel){
+
+}
--- app/src/main/java/com/example/educationgallery/ui/fragments/ScheduleFragment.kt
+++ app/src/main/java/com/example/educationgallery/ui/fragments/ScheduleFragment.kt
@@ -7,10 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.ViewModelProvider
+import com.example.educationgallery.ui.components.ScheduleFragmentScreen
+import com.example.educationgallery.viewmodels.ScheduleViewModel
 
 class ScheduleFragment : Fragment() {
+    private lateinit var viewModel: ScheduleViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[ScheduleViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -19,8 +24,8 @@ class ScheduleFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                MaterialTheme() {
-
+                MaterialTheme {
+                    ScheduleFragmentScreen(viewModel)
                 }
             }
         }
