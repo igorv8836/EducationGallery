9f8ab9d4cd624025685172103e84952a2d600177
Igor <igorv8836@mail.ru>
2024-04-17 03:37:33 +0300

    functions for ScheduleViewModel

--- app/src/main/java/com/example/educationgallery/ui/MainActivity.kt
+++ app/src/main/java/com/example/educationgallery/ui/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.example.educationgallery.ui
 
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Sync
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.educationgallery.R
-import com.example.educationgallery.databinding.ActivityMainBinding
 import com.example.educationgallery.ui.button_navigation.BottomNavigation
 import com.example.educationgallery.ui.button_navigation.NavGraph
 import com.example.educationgallery.viewmodels.MainActivityViewModel
@@ -27,20 +44,81 @@ class MainActivity : AppCompatActivity() {
     private var navController: NavController? = null
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
         setContent {
-            val navController = rememberNavController()
-            Scaffold(
-                bottomBar = { BottomNavigation(navController = navController) }
-            ) {
-                Box(modifier = Modifier.padding(it)) {
-                    NavGraph(navHostController = navController)
+            MaterialTheme {
+                var showSyncDialog by remember { mutableStateOf(false) }
+                var groupText by remember { mutableStateOf("") }
+                val navController = rememberNavController()
+
+                if (showSyncDialog) {
+                    AlertDialog(
+                        onDismissRequest = { showSyncDialog = false },
+                        title = { Text("Синхронизация расписания") },
+                        text = {
+                            Column {
+                                Text(
+                                    "Вы уверены, что хотите синхронизировать расписание?",
+                                    modifier = Modifier.padding(bottom = 8.dp)
+                                )
+                                TextField(
+                                    value = groupText,
+                                    onValueChange = { groupText = it },
+                                    label = { Text("Введите группу") },
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+
+                        },
+                        confirmButton = {
+                            TextButton(onClick = {
+                                showSyncDialog = false
+                                viewModel.syncSchedule(groupText)
+                            }) {
+                                Text("Синхронизировать")
+                            }
+                        },
+                        dismissButton = {
+                            TextButton(onClick = { showSyncDialog = false }) {
+                                Text("Отмена")
+                            }
+                        },
+                    )
+                }
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                            ),
+                            title = {
+                                Text(stringResource(id = R.string.app_name))
+                            },
+                            actions = {
+                                IconButton(onClick = { showSyncDialog = true }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Sync,
+                                        tint = Color.White,
+                                        contentDescription = "Sync icon"
+                                    )
+                                }
+                            },
+                        )
+                    },
+                    bottomBar = { BottomNavigation(navController = navController) }
+                ) {
+                    Box(modifier = Modifier.padding(it)) {
+                        NavGraph(navHostController = navController)
+                    }
                 }
             }
         }
         checkPermission()
-        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
 
     private fun checkPermission() {
--- app/src/main/java/com/example/educationgallery/viewmodels/ScheduleViewModel.kt
+++ app/src/main/java/com/example/educationgallery/viewmodels/ScheduleViewModel.kt
@@ -1,6 +1,48 @@
 package com.example.educationgallery.viewmodels
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class ScheduleViewModel: ViewModel() {
+class ScheduleViewModel : ViewModel() {
+    private val _schedule = MutableStateFlow<List<String>>(emptyList())
+    val schedule = _schedule.asStateFlow()
+
+
+    fun getSchedule() {
+        //...
+        //...
+        // Вы где то получаете данные и тут передаете в _schedule.value расписание,
+    // а в _schedule меняете тип данных на List<Класс расписания> вместо string
+        // _schedule.value = schedule - какое то расписание
+        TODO()
+    }
+
+    fun addSchedule(){
+        TODO("Добавление нового занятия в расписание, параметры будут чуть позже")
+    }
+
+    fun deleteSchedule(){
+        TODO("Удаление занятия из расписания, параметры будут чуть позже")
+    }
+
+    fun changeSchedule(){
+        TODO("Изменение занятия в расписании, параметры будут чуть позже")
+    }
+
+    fun getLessonsTypes(){
+        TODO("Получение типов занятий")
+    }
+
+    fun getLessonTimes(){
+        TODO("Получение времени занятий")
+    }
+
+    fun getFilteredScheduleName(text: String){
+        TODO("Получение списка названий занятий, где есть выражение <text>")
+    }
+
+
+
+
 }
