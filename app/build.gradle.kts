531842b4b0973bcae226441d800a27421127f204
mac <igorv8836@mail.ru>
2024-04-20 13:12:22 +0300

    refactoring build.gradle and coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,30 +47,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.material3:material3-android:1.2.1")
-    implementation("androidx.compose.foundation:foundation-android:1.6.5")
-    implementation("androidx.leanback:leanback:1.0.0")
     val room_version = "2.6.1"
     val nav_version = "2.7.7"
-    val moshi_version = "1.12.0"
-    val retrofit_version = "2.9.0"
     val gson_version = "2.10.1"
     val accompanist_version = "0.28.0"
 
 
-    implementation("androidx.core:core-ktx:+")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
-    implementation("androidx.navigation:navigation-compose:$nav_version")
 
     //Database Converter----------------------------------------------------------------------------
     implementation("com.google.code.gson:gson:$gson_version")
@@ -81,20 +68,17 @@ dependencies {
     implementation("androidx.room:room-ktx:$room_version")
     implementation("androidx.room:room-paging:$room_version")
 
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofit_version")
-
-    implementation("com.squareup.moshi:moshi:$moshi_version")
-    implementation("com.squareup.moshi:moshi-kotlin:$moshi_version")
 
 
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 
     val composeBom = platform("androidx.compose:compose-bom:2024.03.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
     implementation("androidx.compose.material3:material3-android:1.2.1")
-    implementation("androidx.compose.foundation:foundation-android:1.6.5")
+    implementation("androidx.compose.foundation:foundation-android:1.6.6")
     implementation("androidx.leanback:leanback:1.0.0")
 
     implementation("androidx.compose.material3:material3")
@@ -109,8 +93,9 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.compose.runtime:runtime-livedata")
 
     implementation("com.google.accompanist:accompanist-pager:$accompanist_version")
     implementation("com.google.accompanist:accompanist-pager-indicators:$accompanist_version")
+
+    implementation("io.coil-kt:coil-compose:1.4.0")
 }
