1e5662aecee847f5a2303f5914e1362d73ce69a8
igorv8836 <113043399+igorv8836@users.noreply.github.com>
2024-04-21 13:04:01 +0300

    Revert "Realize ScheduleViewModel and MainActivityViewModel. Change some data…"

--- app/src/main/java/com/example/educationgallery/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/educationgallery/viewmodels/MainActivityViewModel.kt
@@ -2,31 +2,10 @@ package com.example.educationgallery.viewmodels
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.educationgallery.model.App
-import com.example.educationgallery.model.Parser
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivityViewModel: ViewModel() {
-    private val tag = "MainActivityVM"
     // Функция, которая синхронизирует расписание согласно группе
-    // TODO: Нужно добавить обновление экрана к Игорю
     fun syncSchedule(group: String){
-        val scheduleDB = App.dataBase
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                scheduleDB.clearAllTables()
-                try {
-                    val parser = Parser("ИКБО-06-22")
-                    parser.fillDB(scheduleDB)
-                }
-                catch (e: Exception){
-                    Log.d(tag, e.message.toString())
-                }
-            }
-        }
-        Log.d(tag, group)
+        Log.d("MainActivityViewModel", group)
     }
 }
